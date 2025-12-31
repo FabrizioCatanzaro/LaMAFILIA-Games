3de2d09e358f366e41fdf4b02b6f6bdede159ffc
Fabrizio Catanzaro <fabriziocatanzaro1@gmail.com>
2025-12-31 15:48:29 -0300

    se empieza a desarrollar el juego PICTIONARY

---
+++ src/data/pictionaryWords.js
@@ -0,0 +1,131 @@
+export const pictionaryWords = [
+    {
+        id: 1,
+        name: "Verbos",
+        words: [
+            "correr","saltar","nadar","volar","bailar","cantar","dibujar","escribir","leer","gritar",
+            "reír","llorar","empujar","tirar","patear","lanzar","agarrar","romper","arreglar","cocinar",
+            "comer","beber","dormir","despertar","manejar","viajar","caminar","trepar","caer","esconderse",
+            "buscar","encontrar","perder","ganar","robar","regalar","abrazar","besar","pelear","pensar",
+            "imaginar","soñar","estudiar","enseñar","aprender","trabajar","descansar","limpiar","ensuciar","lavar",
+            "secar","construir","destruir","pintar","borrar","copiar","pegar","cortar","coser","medir",
+            "pesar","empuñar","apuntar","disparar","huir","perseguir","rescatar","salvar","celebrar","festejar"
+        ]
+    },
+    {
+        id: 2,
+        name: "Objetos",
+        words: [
+            "mesa","silla","puerta","ventana","llave","reloj","celular","computadora","televisor","control remoto",
+            "vaso","plato","tenedor","cuchillo","cuchara","botella","mochila","cuaderno","lapicera","lápiz",
+            "borrador","regla","tijera","pegamento","libro","cama","almohada","sábana","frazada","espejo",
+            "cepillo","peine","toalla","jabón","shampoo","paraguas","cartera","billetera","moneda","billete",
+            "linterna","vela","encendedor","martillo","clavo","destornillador","taladro","candado","cadena","escoba",
+            "pala","balde","manguera","heladera","horno","microondas","licuadora","ventilador","estufa","aire acondicionado",
+            "radio","parlante","auriculares","cámara","trípode","control","teclado","mouse","cable","enchufe"
+        ]
+    },
+    {
+        id: 3,
+        name: "Lugares",
+        words: [
+            "Torre Eiffel","Estatua de la Libertad","Gran Muralla","Coliseo","Big Ben","Pirámides de Egipto","Machu Picchu","Cristo Redentor","Taj Mahal","Times Square",
+            "Disney World","Casa Rosada","Obelisco","Plaza de Mayo","La Bombonera","Monumento a la Bandera","Sagrada Familia","Kremlin","Casa Blanca","Pentágono",
+            "Las Vegas","Nueva York","París","Londres","Roma","Madrid","Barcelona","Tokio","Pekín","Seúl",
+            "Sydney","Río de Janeiro","San Pablo","Ciudad de México","Cancún","Miami","Los Ángeles","Hollywood","Las Cataratas","Ushuaia",
+            "Bariloche","Mar del Plata","Córdoba","Rosario","La Plata","Montevideo","Punta del Este","Santiago de Chile","La Habana","Bogotá",
+            "Medellín","Quito","Lima","Cusco","Amazonas","Sahara","Antártida","Everest","Monte Fuji","Niágara",
+            "Dubái","Abu Dhabi","Venecia","Florencia","Atenas","Santorini","Jerusalén","Vaticano","San Petersburgo","Praga"
+        ]
+    },
+    {
+        id: 4,
+        name: "Personajes",
+        words: [
+            "Messi","Maradona","Mafalda","Gardel","Charly García","Spinetta","Mirta Legrand","Tinelli","Susana","Lali",
+            "Batman","Superman","Spiderman","Iron Man","Hulk","Thor","Capitán América","Joker","Harley Quinn","Deadpool",
+            "Harry Potter","Hermione","Voldemort","Darth Vader","Luke Skywalker","Yoda","Obi Wan","Gandalf","Frodo","Gollum",
+            "Homero Simpson","Bart Simpson","Marge","Lisa","Moe","Bob Esponja","Patricio","Calamardo","Scooby Doo","Shaggy",
+            "Mario","Luigi","Bowser","Peach","Link","Zelda","Pikachu","Ash","Goku","Vegeta",
+            "Naruto","Sasuke","Luffy","Zoro","Sanji","Batman","Robin","Flash","Wonder Woman","Aquaman",
+            "Sherlock Holmes","Dr Watson","James Bond","Indiana Jones","Rocky","Rambo","Terminator","Neo","Matrix","John Wick"
+        ]
+    },
+    {
+        id: 5,
+        name: "Animales",
+        words: [
+            "perro","gato","elefante","jirafa","león","tigre","cebra","hipopótamo","rinoceronte","mono",
+            "gorila","chimpancé","oso","oso polar","panda","lobo","zorro","conejo","ratón","hamster",
+            "vaca","caballo","oveja","cabra","cerdo","gallina","gallo","pollo","pato","ganso",
+            "águila","halcón","búho","paloma","loro","pingüino","avestruz","flamenco","cisne","pez",
+            "tiburón","ballena","delfín","pulpo","calamar","medusa","cangrejo","langosta","estrella de mar","caracol",
+            "serpiente","cobra","víbora","lagarto","camaleón","iguana","tortuga","cocodrilo","rana","sapo",
+            "abeja","avispa","hormiga","mariposa","mosca","mosquito","araña","escorpión","ciempiés","saltamontes"
+        ]
+    },
+    {
+        id: 6,
+        name: "Emociones",
+        words: [
+            "alegría","tristeza","enojo","felicidad","miedo","sorpresa","asco","vergüenza","orgullo","celos",
+            "amor","odio","ansiedad","nervios","calma","paz","estrés","bronca","euforia","nostalgia",
+            "empatía","culpa","confianza","desconfianza","esperanza","frustración","ilusión","timidez","valentía","soledad",
+            "aburrimiento","diversión","tensión","relajación","satisfacción","decepción","motivación","pánico","ternura","envidia",
+            "compasión","admiración","gratitud","melancolía","impaciencia","optimismo","pesimismo","alivio","angustia","placer",
+            "orgasmo","pasión","deseo","atracción","rechazo","confusión","claridad","orgullo","humillación","vergüenza ajena",
+            "culpabilidad","remordimiento","fascinación","shock","susto","pavor","tranquilidad","seguridad","inseguridad","bienestar"
+        ]
+    },
+    {
+        id: 7,
+        name: "Acciones Cotidianas",
+        words: [
+            "despertarse","desayunar","lavarse los dientes","ducharse","vestirse","atarse los zapatos","peinarse","maquillarse","afeitarse","salir",
+            "viajar","esperar el colectivo","manejar","trabajar","estudiar","almorzar","cocinar","lavar platos","limpiar","ordenar",
+            "barrer","trapear","hacer la cama","tender la ropa","doblar ropa","ir al baño","comprar","pagar","hacer fila","cargar nafta",
+            "volver a casa","merendar","mirar televisión","escuchar música","usar el celular","chatear","llamar","mandar audios","sacar fotos","postear",
+            "hacer ejercicio","caminar","correr","ir al gimnasio","estirarse","descansar","dormir siesta","cenar","lavarse la cara","sacarse los zapatos",
+            "acostarse","apagar la luz","poner alarma","soñar","roncar","levantarse de mal humor","llegar tarde","apurarse","esperar","saludar"
+        ]
+    },
+    {
+        id: 8,
+        name: "Profesiones",
+        words: [
+            "médico","enfermero","dentista","psicólogo","kinesiólogo","veterinario","abogado","juez","fiscal","escribano",
+            "docente","maestro","profesor","director","rector","ingeniero","arquitecto","programador","desarrollador","tester",
+            "diseñador","ilustrador","animador","fotógrafo","camarógrafo","periodista","locutor","conductor","actor","actriz",
+            "cantante","músico","compositor","productor","sonidista","técnico","electricista","plomero","gasista","carpintero",
+            "albañil","herrero","soldador","mecánico","chapista","pintor","chofer","taxista","colectivero","piloto",
+            "azafata","mozo","cocinero","chef","panadero","pastelero","heladero","carnicero","verdulero","kiosquero",
+            "policía","bombero","militar","guardavidas","seguridad","portero","administrativo","contador","economista","auditor"
+        ]
+    },
+    {
+        id: 9,
+        name: "Deportes y Juegos",
+        words: [
+            "fútbol","básquet","tenis","voley","handball","rugby","hockey","golf","polo","natación",
+            "atletismo","boxeo","kickboxing","karate","taekwondo","judo","lucha","esgrima","arquería","levantamiento de pesas",
+            "crossfit","gimnasia","yoga","pilates","patín","patinaje artístico","patinaje sobre hielo","ski","snowboard","surf",
+            "windsurf","kitesurf","vela","remo","canotaje","kayak","rafting","buceo","escalada","montañismo",
+            "ciclismo","bmx","mountain bike","motocross","automovilismo","karting","formula 1","enduro","trial","paintball",
+            "airsoft","laser tag","ajedrez","damas","ludo","generala","póker","truco","chinchón","canasta",
+            "ping pong","metegol","bowling","pool","snooker","tejo","rayuela","saltar la soga","escondidas","mancha"
+        ]
+    },
+    {
+        id: 10,
+        name: "Comidas y Bebidas",
+        words: [
+            "pizza","empanada","hamburguesa","choripán","asado","milanesa","pastas","ravioles","ñoquis","fideos",
+            "lasagna","sushi","arroz","pollo","carne","pescado","ensalada","sopa","guiso","estofado",
+            "tarta","tortilla","omelette","pan","tostada","factura","medialuna","bizcochuelo","torta","budín",
+            "helado","flan","gelatina","chocolate","alfajor","galletitas","donas","brownie","panqueque","waffle",
+            "café","té","mate","tereré","jugo","licuado","batido","gaseosa","cerveza","vino",
+            "champagne","fernet","whisky","vodka","ron","gin","licor","sidra","agua","agua con gas",
+            "papas fritas","puré","arroz con leche","polenta","humita","locro","carbonada","tamales","paella","risotto"
+        ]
+    },
+]
---
+++ src/screens/pictionary/PictionaryConfigScreen.js
@@ -0,0 +1,360 @@
+import React, { useState } from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  ScrollView,
+  TextInput,
+  TouchableOpacity,
+  Alert,
+} from 'react-native';
+import { pictionaryWords } from '../../data/pictionaryWords';
+
+export default function PictionaryConfigScreen({ navigation, route }) {
+  const previousConfig = route.params?.previousConfig;
+
+  // Reiniciar IDs de equipos para evitar conflictos
+  const normalizedTeams = previousConfig?.teams 
+    ? previousConfig.teams.map((team, index) => ({
+        ...team,
+        id: index + 1,
+        score: 0
+      }))
+    : [
+        { id: 1, name: 'Equipo 1', score: 0 },
+        { id: 2, name: 'Equipo 2', score: 0 },
+      ];
+      
+  const [teams, setTeams] = useState(normalizedTeams);
+  const [selectedCategories, setSelectedCategories] = useState(
+    previousConfig?.selectedCategories || [1]
+  );
+  const [roundDuration, setRoundDuration] = useState(
+    previousConfig?.roundDuration || 60
+  );
+  const [numberOfRounds, setNumberOfRounds] = useState(
+    previousConfig?.totalRounds || 3
+  );
+
+  // Agregar equipo
+  const addTeam = () => {
+    if (teams.length < 8) {
+      // Encontrar el ID máximo actual y sumar 1
+      const maxId = teams.length > 0 ? Math.max(...teams.map(t => t.id)) : 0;
+      setTeams([
+        ...teams,
+        { id: maxId + 1, name: `Equipo ${teams.length + 1}`, score: 0 },
+      ]);
+    } else {
+      Alert.alert('Límite alcanzado', 'Máximo 8 equipos');
+    }
+  };
+
+  // Eliminar equipo
+  const removeTeam = (id) => {
+    if (teams.length > 2) {
+      setTeams(teams.filter((t) => t.id !== id));
+    } else {
+      Alert.alert('Error', 'Debe haber al menos 2 equipos');
+    }
+  };
+
+  // Actualizar nombre de equipo
+  const updateTeamName = (id, name) => {
+    setTeams(teams.map((t) => (t.id === id ? { ...t, name } : t)));
+  };
+
+  // Toggle categoría
+  const toggleCategory = (categoryId) => {
+    if (selectedCategories.includes(categoryId)) {
+      if (selectedCategories.length > 1) {
+        setSelectedCategories(selectedCategories.filter((id) => id !== categoryId));
+      } else {
+        Alert.alert('Error', 'Debe haber al menos una opción de categoría seleccionada');
+      }
+    } else {
+      setSelectedCategories([...selectedCategories, categoryId]);
+    }
+  };
+
+  // Comenzar juego
+  const startGame = () => {
+    // Validar nombres de equipos
+    const validTeams = teams.filter((t) => t.name.trim() !== '');
+    if (validTeams.length < 2) {
+      Alert.alert('Error', 'Debe haber al menos 2 equipos con nombres');
+      return;
+    }
+
+    // Verificar nombres duplicados
+    const uniqueNames = new Set(validTeams.map((t) => t.name));
+    if (uniqueNames.size !== validTeams.length) {
+      Alert.alert('Error', 'No puede haber nombres de equipos duplicados');
+      return;
+    }
+
+        // Ir a pantalla pre-juego en lugar de directo al juego
+    navigation.navigate('TabuPreGame', {
+      teams: validTeams,
+      selectedCategories,
+      roundDuration,
+      currentTeamIndex: 0,
+      currentRound: 1,
+      totalRounds: numberOfRounds,
+    });
+  };
+
+  return (
+    <ScrollView style={styles.container}>
+      {/* Equipos */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Equipos ({teams.length})</Text>
+        <Text style={styles.sectionSubtitle}>Mínimo 2 equipos, máximo 6</Text>
+
+        {teams.map((team) => (
+          <View key={team.id} style={styles.teamRow}>
+            <TextInput
+              style={styles.teamInput}
+              placeholder={`Equipo ${team.id}`}
+              value={team.name}
+              onChangeText={(text) => updateTeamName(team.id, text)}
+            />
+            <TouchableOpacity
+              style={[styles.deleteButton, teams.length === 2 && styles.deleteButtonDisabled]}
+              onPress={() => removeTeam(team.id)}
+              disabled={teams.length === 2}
+            >
+              <Text style={[styles.deleteIcon, teams.length === 2 && styles.iconDisabled]}>
+                ✕
+              </Text>
+            </TouchableOpacity>
+          </View>
+        ))}
+
+        <TouchableOpacity
+          style={[styles.addButton, teams.length >= 6 && styles.addButtonDisabled]}
+          onPress={addTeam}
+          disabled={teams.length >= 6}
+        >
+          <Text style={styles.addButtonText}>+ Agregar Equipo</Text>
+        </TouchableOpacity>
+      </View>
+
+      {/* Categorías */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Categorías</Text>
+        {tabuCategories.map((category) => (
+          <TouchableOpacity
+            key={category.id}
+            style={[
+              styles.categoryButton,
+              selectedCategories.includes(category.id) && styles.categoryButtonActive,
+            ]}
+            onPress={() => toggleCategory(category.id)}
+          >
+            <Text
+              style={[
+                styles.categoryButtonText,
+                selectedCategories.includes(category.id) && styles.categoryButtonTextActive,
+              ]}
+            >
+              {category.name} ({category.words.length} palabras)
+            </Text>
+          </TouchableOpacity>
+        ))}
+      </View>
+
+      {/* Duración de ronda */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Duración por Ronda (segundos)</Text>
+        <View style={styles.durationButtons}>
+          {[30, 60, 90, 120].map((duration) => (
+            <TouchableOpacity
+              key={duration}
+              style={[
+                styles.durationButton,
+                roundDuration === duration && styles.durationButtonActive,
+              ]}
+              onPress={() => setRoundDuration(duration)}
+            >
+              <Text
+                style={[
+                  styles.durationButtonText,
+                  roundDuration === duration && styles.durationButtonTextActive,
+                ]}
+              >
+                {duration}s
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+
+      {/* Cantidad de Rondas */}
+      <View style={styles.section}>
+        <Text style={styles.sectionTitle}>Cantidad de Rondas</Text>
+        <Text style={styles.sectionSubtitle}>
+          Cada ronda todos los equipos juegan una vez
+        </Text>
+        <View style={styles.durationButtons}>
+          {[1, 2, 3, 4, 5, 6, 7, 8].map((rounds) => (
+            <TouchableOpacity
+              key={rounds}
+              style={[
+                styles.durationButton,
+                numberOfRounds === rounds && styles.durationButtonActive,
+              ]}
+              onPress={() => setNumberOfRounds(rounds)}
+            >
+              <Text
+                style={[
+                  styles.durationButtonText,
+                  numberOfRounds === rounds && styles.durationButtonTextActive,
+                ]}
+              >
+                {rounds}
+              </Text>
+            </TouchableOpacity>
+          ))}
+        </View>
+      </View>
+
+      {/* Botón Comenzar */}
+      <TouchableOpacity style={styles.startButton} onPress={startGame}>
+        <Text style={styles.startButtonText}>Comenzar Juego</Text>
+      </TouchableOpacity>
+
+      <View style={{ height: 30 }} />
+    </ScrollView>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#f5f5f5',
+  },
+  section: {
+    backgroundColor: '#fff',
+    padding: 15,
+    marginVertical: 8,
+    marginHorizontal: 10,
+    borderRadius: 10,
+    elevation: 2,
+  },
+  sectionTitle: {
+    fontSize: 18,
+    fontWeight: 'bold',
+    marginBottom: 5,
+    color: '#333',
+  },
+  sectionSubtitle: {
+    fontSize: 14,
+    color: '#666',
+    marginBottom: 10,
+  },
+  teamRow: {
+    flexDirection: 'row',
+    alignItems: 'center',
+    marginBottom: 10,
+  },
+  teamInput: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ddd',
+    borderRadius: 8,
+    padding: 12,
+    fontSize: 16,
+  },
+  deleteButton: {
+    width: 40,
+    height: 40,
+    justifyContent: 'center',
+    alignItems: 'center',
+    marginLeft: 10,
+  },
+  deleteButtonDisabled: {
+    opacity: 0.3,
+  },
+  deleteIcon: {
+    fontSize: 24,
+    color: '#e74c3c',
+  },
+  iconDisabled: {
+    color: '#ccc',
+  },
+  addButton: {
+    backgroundColor: '#3498db',
+    padding: 12,
+    borderRadius: 8,
+    alignItems: 'center',
+    marginTop: 5,
+  },
+  addButtonDisabled: {
+    backgroundColor: '#95a5a6',
+    opacity: 0.5,
+  },
+  addButtonText: {
+    color: '#fff',
+    fontSize: 16,
+    fontWeight: 'bold',
+  },
+  categoryButton: {
+    padding: 12,
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: '#ddd',
+    marginBottom: 8,
+    alignItems: 'center',
+  },
+  categoryButtonActive: {
+    borderColor: '#3498db',
+    backgroundColor: '#3498db',
+  },
+  categoryButtonText: {
+    fontSize: 16,
+    color: '#666',
+  },
+  categoryButtonTextActive: {
+    color: '#fff',
+    fontWeight: 'bold',
+  },
+  durationButtons: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  durationButton: {
+    flex: 1,
+    padding: 15,
+    marginHorizontal: 3,
+    borderRadius: 8,
+    borderWidth: 2,
+    borderColor: '#ddd',
+    alignItems: 'center',
+  },
+  durationButtonActive: {
+    borderColor: '#f39c12',
+    backgroundColor: '#f39c12',
+  },
+  durationButtonText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    color: '#666',
+  },
+  durationButtonTextActive: {
+    color: '#fff',
+  },
+  startButton: {
+    backgroundColor: '#2ecc71',
+    padding: 18,
+    margin: 15,
+    borderRadius: 10,
+    alignItems: 'center',
+    elevation: 5,
+  },
+  startButtonText: {
+    color: '#fff',
+    fontSize: 18,
+    fontWeight: 'bold',
+  },
+});
